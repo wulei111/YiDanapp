fef5f2a4c8118c78e074ff0f8ee4769449f4a663
wulei111 <1395995359@qq.com>
2015-12-16 13:31:37 +0800

    Main

---
+++ YiDanApp/app/src/main/java/com/best/yidanapp/adapter/ListAdapter.java
@@ -0,0 +1,7 @@
+package com.best.yidanapp.adapter;
+
+/**
+ * Created by Administrator on 2015/12/16.
+ */
+public class ListAdapter {
+}
---
+++ YiDanApp/app/src/main/java/com/best/yidanapp/bean/Login.java
@@ -0,0 +1,7 @@
+package com.best.yidanapp.bean;
+
+/**
+ * Created by Administrator on 2015/12/16.
+ */
+public class Login {
+}
